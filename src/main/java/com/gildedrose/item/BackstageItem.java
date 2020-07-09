c77fbe017ce26a5878a467ddd799c6dbdde50774
zampa00 <zampa92@gmail.com>
2020-07-09 17:09:12 +0200

    backstageTicketsDropsTo0AfterConcert green

--- src/main/java/com/gildedrose/item/BackstageItem.java
+++ src/main/java/com/gildedrose/item/BackstageItem.java
@@ -12,7 +12,7 @@ public class BackstageItem extends AbstractItem {
     public void update() {
         this.sellIn--;
         if (this.sellIn < 0)
-            this.quality += 2;
+            this.quality = 0;
         else if (this.sellIn < 5)
             this.quality += 3;
         else if (this.sellIn < 10)
